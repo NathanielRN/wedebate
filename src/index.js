df1d7c39d169a0ce5eda6bbf9b486b761feb3ccc
Eliseo Nathaniel Ruiz Nowell <nathanielruiz98@gmail.com>
2018-06-02 19:57:56 -0700

    Used cli to create react app, brought everything to bare website skeleton

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+  // This is the main app component
+  <App />,
+
+  // Here we say we want to place our "App" object where the root id element is
+  document.getElementById('root')
+);
+registerServiceWorker();
